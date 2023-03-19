9bd5bbe6cdc1ea84dfa20a793af8fdb04fc1b5d3
stanley <stanley6453@gmail.com>
2023-03-19 12:55:49 +0100

    modified filestorage added function delete and modified function all to support filtering

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,12 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+        return {key: value for key, value in FileStorage.__objects.items()
+                if cls.__name__ == key.split('.')[0]}
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -24,6 +27,14 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        """ delete obj from __objects if it’s inside"""
+        if obj is None:
+            return
+        for key, value in FileStorage.__objects.items():
+            if obj is value:
+                del FileStorage.__objects[key]
 
     def reload(self):
         """Loads storage dictionary from file"""
