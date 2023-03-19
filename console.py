721f2cc71b8a0881dc285a3c6ab79cf584f1d32b
stanley <stanley6453@gmail.com>
2023-03-19 09:51:30 +0100

    modified some files

--- console.py
+++ console.py
@@ -224,13 +224,14 @@ class HBNBCommand(cmd.Cmd):
             for k, v in storage._FileStorage__objects.items():
                 print_list.append(str(v))
 
-        output = ''
-        print('[', end='')
-        for item in print_list:
-            output += item + ', '
-        output = output[:-2]
-        print(output, end='')
-        print(']')
+        print(print_list)
+        # output = ''
+        # print('[', end='')
+        # for item in print_list:
+        #     output += item + ', '
+        # output = output[:-2]
+        # print(output, end='')
+        # print(']')
 
     def help_all(self):
         """ Help information for the all command """
