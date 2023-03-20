b0955569c31ad5e24b7bb8c2ba90fb9fd89d0998
stanley <stanley6453@gmail.com>
2023-03-20 04:20:01 +0100

    0x02. AirBnB clone - MySQL Task 6. DBStorage - States and Cities
    created models/engine/db_storage.py as the interface for our database storage and modified models/__init__.p to support both database and file storage based on the environment variable HBNB_TYPE_STORAGE

--- models/base_model.py
+++ models/base_model.py
@@ -1,41 +1,29 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
+from models import storage
 from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 
-
-def dict_convert_no_value(kwargs):
-    for key, value in kwargs.items():
-        if key not in ['city_id', 'user_id', 'created_at', 'updated_at']\
-                and type(value) not in [int, float, datetime]:
-            if value.isnumeric():
-                kwargs[key] = int(value)
-            elif isfloat(value):
-                kwargs[key] = float(value)
-
-    return kwargs
-
-
-def isfloat(num):
-    try:
-        float(num)
-        return True
-    except ValueError:
-        return False
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    
+    id = Column(String(60), primary_key=True)
+    cur_date = datetime.utcnow()
+    created_at = Column(DateTime, nullable=False, default=cur_date)
+    updated_at = Column(DateTime, nullable=False, default=cur_date)
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        from models import storage
         new = None
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
             if kwargs.get('updated_at') is not None:
                 kwargs['updated_at'] =\
@@ -59,9 +47,6 @@ class BaseModel:
             kwargs = dict_convert_no_value(kwargs)
             self.__dict__.update(kwargs)
 
-            if new is not None:
-                storage.new(self)
-
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
@@ -71,7 +56,12 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
+
+    def delete(self):
+        """delete the current instance from the storage (models.storage)"""
+        storage.delete(self)
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -81,4 +71,26 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if dictionary.get('_sa_instance_state') is not None:
+            del dictionary['_sa_instance_state']
         return dictionary
+
+
+def dict_convert_no_value(kwargs):
+    for key, value in kwargs.items():
+        if key not in ['city_id', 'user_id', 'created_at', 'updated_at']\
+                and type(value) not in [int, float, datetime]:
+            if value.isnumeric():
+                kwargs[key] = int(value)
+            elif isfloat(value):
+                kwargs[key] = float(value)
+
+    return kwargs
+
+
+def isfloat(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
---
+++ models/engine/db_storage.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""Database storage engine for Airbnb project"""
+from os import getenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import Base
+
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+
+class DBStorage():
+    """Database storage engine class for Airbnb project"""
+    _engine = None
+    _session = None
+
+    def __init__(self):
+        """Initialize the Database storage"""
+        user = getenv('HBNB_MYSQL_USER', 'hbnb_dev')
+        password = getenv('HBNB_MYSQL_PWD')
+        host = getenv('HBNB_MYSQL_HOST', 'localhost')
+        database = getenv('HBNB_MYSQL_DB', 'hbnb_dev_db')
+
+        self.__engine = create_engine(f'mysql+mysqldb://\
+                {user}:{password}@{host}/{database}', pool_pre_ping=True)
+
+        if (getenv('HBNB_ENV') == 'test'):
+            Base.metadata.drop_all(bind=self.__engine)
+        
+    def all(self, cls=None):
+        """query on the current database session (self.__session) all objects
+        depending of the class name (argument cls)"""
+        if cls:
+            self.__session.query(cls)
+        else:
+            self.__session.query(User)
+            self.__session.query(State)
+            self.__session.query(City)
+            self.__session.query(Amenity)
+            self.__session.query(Place)
+            self.__session.query(Review)
+
+    def new(self, obj):
+        """add the object to the current database session"""
+        self.__session.add(obj)
+
+    def save(self):
+        """commit all changes of the current database session"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """delete from the current database session obj if not None"""
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        Base.metadata.create_all(bind=self.__engine)
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False, )
+        Session = scoped_session(session_factory)
+        self.__session = Session()
