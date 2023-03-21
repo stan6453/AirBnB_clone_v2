051213b44236add5d4d1b037fb732e45476e8e39
stanley <stanley6453@gmail.com>
2023-03-21 05:47:07 +0100

    0x02. AirBnB clone - MySQL Task 10. DBStorage - Amenity... and BOOM!
    updated models/amenity.py to support sqlalchemy object to database table mapping, defined a many to many relationship between amenities and places using the places_amenities table.

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """
     Amenities Class representing amenities a place
     can offer
