0825de3273dd2c247ffa348bbef62c278ffa5c4d
stanley <stanley6453@gmail.com>
2023-03-21 05:45:20 +0100

    0x02. AirBnB clone - MySQL Task 10. DBStorage - Amenity... and BOOM!
    updated models/amenity.py to support sqlalchemy object to database table mapping, defined a many to many relationship between amenities and places using the places_amenities table.

--- models/place.py
+++ models/place.py
@@ -22,9 +22,10 @@ class Place(BaseModel, Base):
     amenity_ids = []
 
     place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60), ForeignKey(
-                              'places.id'), primary_key=True),
-                          Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True))
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id'), primary_key=True),
+                          Column('amenity_id', String(60), ForeignKey('amenities.id'),
+                                 primary_key=True))
 
     '''amenities for DBStorage'''
     amenities = relationship('Amenity', backref='places',
