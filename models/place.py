a74db922fdb8c921b891cd6e34370439a23ef5f9
stanley <stanley6453@gmail.com>
2023-03-22 17:40:18 +0100

    0x02. AirBnB clone - MySQL Task. Error fix. Separated all model classes' properties between ObjectStorage and DBStorage

--- models/place.py
+++ models/place.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, Table, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import Table
 from os import getenv
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
