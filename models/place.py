a44ef15aeecbcfa32a831578004bb9768a3db6c8
stanley <stanley6453@gmail.com>
2023-03-22 16:37:13 +0100

    0x02. AirBnB clone - MySQL Task. Error fix. Separated all model classes' properties between ObjectStorage and DBStorage

--- models/place.py
+++ models/place.py
@@ -5,11 +5,12 @@ from sqlalchemy import Column, String, Integer, Float, Table, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
 
-place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id', String(60),
-                             ForeignKey('places.id'), primary_key=True),
-                      Column('amenity_id', String(60),
-                             ForeignKey('amenities.id'), primary_key=True))
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    place_amenity = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id'), primary_key=True),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id'), primary_key=True))
 
 
 class Place(BaseModel, Base):
