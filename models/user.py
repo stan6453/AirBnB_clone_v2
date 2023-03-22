db379bbfb9ce89caf896b43551489ac285f83f79
stanley <stanley6453@gmail.com>
2023-03-22 15:51:45 +0100

    0x02. AirBnB clone - MySQL Task. Error fix. Separated all model classes' properties between ObjectStorage and DBStorage

--- models/user.py
+++ models/user.py
@@ -9,12 +9,18 @@ class User(BaseModel, Base):
     '''User Class that inherits from BaseModel'''
     __tablename__ = "users"
 
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
 
-    places = relationship('Place', backref='user',
-                          cascade='all, delete, delete-orphan')
-    reviews = relationship('Review', backref='user',
-                           cascade='all, delete, delete-orphan')
+        places = relationship('Place', backref='user',
+                              cascade='all, delete, delete-orphan')
+        reviews = relationship('Review', backref='user',
+                               cascade='all, delete, delete-orphan')
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
