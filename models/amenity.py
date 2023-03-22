d9b8aa7950ead8e8f789f811c7a5e2184aee1451
stanley <stanley6453@gmail.com>
2023-03-22 16:21:39 +0100

    0x02. AirBnB clone - MySQL Task. Error fix. Separated all model classes' properties between ObjectStorage and DBStorage

--- models/amenity.py
+++ models/amenity.py
@@ -14,11 +14,6 @@ class Amenity(BaseModel, Base):
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
-
-        place_amenity = Table('place_amenity', Base.metadata,
-                              Column('place_id', String(60),
-                                     ForeignKey('places.id'), primary_key=True),
-                              Column('amenity_id', String(60),
-                                     ForeignKey('amenities.id'), primary_key=True))
+        
     else:
         name = ""
