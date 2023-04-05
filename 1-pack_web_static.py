5974147c387092f8da82e62995a54bfc230db615
stanley <stanley6453@gmail.com>
2023-04-05 19:11:45 +0100

    added file 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,6 +4,7 @@ generates a .tgz archive from the contents of the web_static
 """
 
 from fabric.api import local, lcd
+from datetime import datetime
 
 
 def do_pack():
@@ -14,5 +15,4 @@ def do_pack():
     date_string = date.strftime('%Y%m%d%H%M%S')
     output = f"web_static_{date_string}.tgz"
     local('mkdir -p ./versions')
-    with lcd('./versions'):
-        local(f'tar -xzvf {output} ../web_static')
+    local(f'tar -cvzf versions/{output} web_static')
