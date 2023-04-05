f5d3c5a84efd0617ef7fe43236a2eeed617e0761
stanley <stanley6453@gmail.com>
2023-04-05 18:41:44 +0100

    added file 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""generates a .tgz archive from the contents of the web_static"""
+"""
+generates a .tgz archive from the contents of the web_static
+"""
 
 from fabric.api import local, lcd
 from datetime import datetime
 
 
 def do_pack():
-    """save a conpressed version of webstatic"""
+    """
+    save a compressed version of web_static
+    """
     date = datetime.now()
     date_string = date.strftime("%Y%m%d%H%M%S")
     output = f"web_static_{date_string}.tgz"
