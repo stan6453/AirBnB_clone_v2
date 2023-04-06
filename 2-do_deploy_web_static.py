910f71aba40c4d76dc3e804771778d955eb588de
stanley <stanley6453@gmail.com>
2023-04-06 07:21:44 +0100

    added file 2-do_deploy_web_static.py

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+generates a .tgz archive from the contents of the web_static
+"""
+
+from fabric.api import *
+from datetime import datetime
+from os import path
+
+env.hosts = ['35.153.83.180', '18.209.223.169']
+env.user = "ubuntu"
+
+
+def do_pack():
+    """
+    save a compressed version of web_static
+    """
+    date = datetime.now()
+    date_string = date.strftime('%Y%m%d%H%M%S')
+    output = "web_static_{}.tgz".format(date_string)
+    local('mkdir -p versions')
+    stat = local('tar -cvzf versions/{} web_static'.format(output))
+    if stat.succeeded:
+        return 'versions/{}'.format(output)
+    else:
+        return None
+
+
+def do_deploy(archive_path):
+    if local('[[ -e {} ]]'.format(archive_path)).failed:
+        return False
+    if put(archive_path, "/tmp").failed:
+        return False
+    basename = path.basename(archive_path)
+    filename = basename.split('.')[0]
+    if run("mkdir -p /data/web_static/releases/{}".format(filename)).failed:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}"
+           .format(basename, filename)).failed:
+        return False
+    if run("rm -rf /tmp/{}".format(basename)).failed:
+        return False
+    if run("rm -rf /data/web_static/current").failed:
+        return False
+    if run("ln -s /data/web_static/releases/{} /data/web_static/current"
+           .format(filename)).failed:
+        return False
