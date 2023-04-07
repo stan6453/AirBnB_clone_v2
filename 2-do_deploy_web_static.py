4fc81b282c2be7832ec0555d6cfaa34b836c8fdb
stanley <stanley6453@gmail.com>
2023-04-07 04:30:42 +0100

    bug fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,13 +3,11 @@
 generates a .tgz archive from the contents of the web_static
 """
 
-from fabric.api import *
+from fabric.api import local, put, run, env
 from datetime import datetime
 from os import path
 
 env.hosts = ['35.153.83.180', '18.209.223.169']
-env.user = 'ubuntu'
-env.key_filename = ['./school', '/school', '~/.ssh/school']
 
 
 def do_pack():
@@ -33,25 +31,27 @@ def do_deploy(archive_path):
     """
     basename = path.basename(archive_path)
     filename = basename.split('.')[0]
-    with cd('/'):
-        if local("file ./{}".format(archive_path)).failed:
-            return False
-        if put("./{}".format(archive_path), "/tmp/", use_sudo=True, temp_dir="~/").failed:
-            return False
-        if run("mkdir -p /data/web_static/releases/{}".format(filename)).failed:
-            return False
-        if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}"
-            .format(basename, filename)).failed:
-            return False
-        if run("rm -rf /tmp/{}".format(basename)).failed:
-            return False
-        if run("mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/".format(filename, filename)).failed:
-            return False
-        if run("rm -rf /data/web_static/releases/{}/web_static".format(filename)).failed:
-            return False
-        if run("rm -rf /data/web_static/current").failed:
-            return False
-        if run("ln -s /data/web_static/releases/{} /data/web_static/current"
-            .format(filename)).failed:
-            return False
+    if local("file ./{}".format(archive_path)).failed:
+        return False
+    if put("{}".format(archive_path), "/tmp/", use_sudo=True).failed:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}".format(filename)).failed:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}"
+           .format(basename, filename)).failed:
+        return False
+    if run("rm -rf /tmp/{}".format(basename)).failed:
+        return False
+    if run(
+        "mv /data/web_static/releases/{}/web_static/* \
+        /data/web_static/releases/{}/".format(filename, filename)
+    ).failed:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static"
+           .format(filename)).failed:
+        return False
+    if run("rm -rf /data/web_static/current").failed:
+        return False
+    if run("ln -s /data/web_static/releases/{} /data/web_static/current"
+           .format(filename)).failed:
+        return False
