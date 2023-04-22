74616fe02b8024db4506a4089d07f5cfc13ac3f4
stanley <stanley6453@gmail.com>
2023-04-22 14:33:53 +0100

    created file web_flask/7-states_list.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""This module instantiates an instance of the Storage will be used"""
+
 from os import getenv
 
-storage = None
-if getenv('HBNB_TYPE_STORAGE') == 'db':
+storage_type = getenv('HBNB_TYPE_STORAGE')
+
+if storage_type == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
