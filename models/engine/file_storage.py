5dd5b90b00215a0789cba2479aae5645accaa4a9
stanley <stanley6453@gmail.com>
2023-04-22 07:47:31 +0100

    created file web_flask/7-states_list.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -59,7 +59,8 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                    self.__objects[key] = classes[val['__class__']](**val)
+                    FileStorage.__objects[key] = classes[val['__class__']](
+                        **val)
         except FileNotFoundError:
             pass
 
