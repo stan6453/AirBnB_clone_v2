05992b4a5c065b9c803cfd3b79b40b3762cdc390
stanley <stanley6453@gmail.com>
2023-04-22 05:32:12 +0100

    created file web_flask/7-states_list.py

--- models/base_model.py
+++ models/base_model.py
@@ -20,34 +20,15 @@ class BaseModel:
         updated_at = Column(DateTime, nullable=False, default=cur_date)
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        new = None
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
-            if kwargs.get('updated_at') is not None:
-                kwargs['updated_at'] =\
-                    datetime.strptime(
-                        kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            else:
-                self.updated_at = datetime.now()
-            if kwargs.get('created_at') is not None:
-                kwargs['created_at'] =\
-                    datetime.strptime(kwargs['created_at'],
-                                      '%Y-%m-%dT%H:%M:%S.%f')
-            else:
-                self.created_at = datetime.now()
-            if kwargs.get('__class__') is not None:
-                del kwargs['__class__']
-
-            if kwargs.get('id') is None:
-                self.id = str(uuid.uuid4())
-                new = True
-
-            kwargs = dict_convert_no_value(kwargs)
-            self.__dict__.update(kwargs)
+        """Instantiates a new model"""
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.now()
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == 'created_at' or k == 'updated_at':
+                    v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                if k != "__class__":
+                    setattr(self, k, v)
 
     def __str__(self):
         """Returns a string representation of the instance"""
--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -17,7 +17,7 @@ def tearItDown(exception):
 @app.route('/states_list', strict_slashes=False)
 def list_states():
     """/states_list route"""
-    states = storage.all('State')
+    states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
